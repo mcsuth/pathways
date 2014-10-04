62a6c14a058086db0e26c3a01728717514905802
mcsuth <seimith@gmail.com>
2014-10-04 08:57:38 -0700

    Add comments

--- public/script.js
+++ public/script.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
   }
 
   // ******************* TOGGLE DOWN jQUERY ************** //
+  // This section contains the functions (2) that control  //
+  // the toggling up/down of the form fields.              //
   // ***************************************************** //
   $("#toggledrawer").click(function(){
     $('#menu').slideToggle();
@@ -21,21 +23,31 @@ $(document).ready(function(){
   });
 
   // ******************* TOGGLE UP jQUERY **************** //
+  // This section will trigger the AJAX (1) request.       //
   // ***************************************************** //
   $('#message2').on('click', function(){
     log("AJAX request to GET")
   })
 
   // ******************* ADD BUTTON ********************** //
+  // This section contains a randomizer to generate        //
+  // 'fun phrases' when the user the function to add more  //
   // ***************************************************** //
   var counter = 0;
   var cloned;
-
   $('.destinations').on('click', '.add', function() {
     counter++;
+    var funphrases = {
+      0: "Then I\'m going to",
+      1: "Later, I\'m going to bounce to", 
+      2: "Later, zooming off to", 
+      3: "Then, I\'m going to split it to",
+      4: "Then, peacing-out to"
+    };
+    var randomfunphrase = Math.floor(Math.random() * 4 + 0);
     var templateDestination = '<div class="destination'+counter+'">'
     + '<h1>'
-    + 'Then, I\'m going to'
+    + funphrases[randomfunphrase]
     + '<input type="text" value="Golden Gate Bridge">'
     + '<div class="actions" style="display: inline">'
     + '<a class="add btn btn-primary"> [ + ]</a> '
@@ -45,13 +57,18 @@ $(document).ready(function(){
     + '</div>'
     $(templateDestination).appendTo('form.destinations')
     $(this).closest('div').find('.add').hide()
-    // debugger
+    log( Math.floor(Math.random() * 4 + 0) )
   })
+  // ******************* ADD BUTTON ********************** //
+  // This section contains the function to remove input    //
+  // fields and reshow the 'add' button to the previous    //
+  // div.                                                  //
+  // ***************************************************** //
   $('.destinations').on('click', '.remove', function() {
     $(this).parent().parent().closest('div').remove();
-    if ( $('form.destinations >').length === 1) {
-      $('.add').show()
-    }
+    // if ( $('form.destinations >').length === 1) {
+    //   $('.add').show()
+    // }
     $('form.destinations >').last().find('.add').show()
   })
 });
