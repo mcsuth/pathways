f12ec6239e1848b14a407d02e12c1078f66ad4cc
mcsuth <seimith@gmail.com>
2014-10-09 07:21:49 -0700

    Clean spacing

--- public/js/script.js
+++ public/js/script.js
@@ -59,18 +59,19 @@ $(document).ready(function(){
     };
     var randomfunphrase = Math.floor(Math.random() * 4 + 0);
     var templateDestination = '<div class="destination'+counter+'">'
-    + '<h1>'
-    + funphrases[randomfunphrase]
-    + '<input type="text" value="Golden Gate Bridge">'
-    + '<div class="actions" style="display: inline">'
-    + '<a class="add btn btn-primary"> [ + ]</a> '
-    + '<a class="remove btn btn-primary">[ - ]</a>'
-    + '</div>'
-    + '</h1>'
-    + '</div>'
+      + '<h1>'
+      + funphrases[randomfunphrase]
+      + '<input type="text" value="Golden Gate Bridge">'
+      + '<div class="actions" style="display: inline">'
+      + '<a class="add btn btn-primary"> [ + ]</a> '
+      + '<a class="remove btn btn-primary">[ - ]</a>'
+      + '</div>'
+      + '</h1>'
+      + '</div>'
     $(templateDestination).appendTo('form.destinations')
     $(this).closest('div').find('.add').hide()
   })
+  
   // ******************* ADD BUTTON ********************** //
   // This section contains the function to remove input    //
   // fields and reshow the 'add' button to the previous    //
